714c088cec9b9ae951449fd367c57f6c35b6813c
Debraj-Das <debrajdas.gayatri@gmail.com>
2024-08-21 20:36:04 +0530

    public files access in api routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 app.use(cors());
 
